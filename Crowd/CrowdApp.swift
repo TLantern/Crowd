1b34bfbd4f0b331fad9b209622c24b73a9197d97
Ten <pgtherealmvp@gmail.com>
2025-10-18 23:26:28 -0500

    Initial Commit

---
+++ Crowd/CrowdApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrowdApp.swift
+//  Crowd
+//
+//  Created by Teni Owojori on 10/18/25.
+//
+
+import SwiftUI
+
+@main
+struct CrowdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CrowdTests/CrowdTests.swift
@@ -0,0 +1,17 @@
+//
+//  CrowdTests.swift
+//  CrowdTests
+//
+//  Created by Teni Owojori on 10/18/25.
+//
+
+import Testing
+@testable import Crowd
+
+struct CrowdTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
