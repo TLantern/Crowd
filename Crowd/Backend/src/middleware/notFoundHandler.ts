8dce1d8ad5b728ad4c67a31cd52202a9bfed9a10
Ten <pgtherealmvp@gmail.com>
2025-10-23 00:51:33 -0500

    Removing Backend for now

--- Crowd/Backend/src/middleware/notFoundHandler.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-
-export const notFoundHandler = (req: Request, res: Response, next: NextFunction): void => {
-  const error = new Error(`Not Found - ${req.originalUrl}`);
-  res.status(404);
-  next(error);
-};
-
